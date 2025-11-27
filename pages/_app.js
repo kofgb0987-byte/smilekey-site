738fa1681e38c359b7789ec8854c66eb50c8e4ed
HyeongeonYu <yuhyeongeon@gmail.com>
2025-11-27 18:36:01 +0900

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react" // or /next
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
